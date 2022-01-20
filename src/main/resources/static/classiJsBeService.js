28a7509f756a53185679a1d1adf678a21c8f7984
Diego <diego.cappai@tiscali.it>
2022-01-20 10:05:22 +0100

    inserimento fatturato 1.2

---
+++ src/main/resources/static/classiJsBeService.js
@@ -0,0 +1,18 @@
+
+class Fattura {
+
+    constructor(data, numeroFattura, anno, importo) {
+        this.data = data;
+        this.numeroFattura = numeroFattura;
+        this.anno = anno;
+        this.importo = importo;
+    }
+}
+
+class StatoFattura {
+
+    constructor(id , nome){
+        this.id = id;
+        this.nome = nome;
+    }
+}
